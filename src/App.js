2cb973c20c81b51200461fdbc5302b92e8032ebb
Ramana Naidu <63719686+Ramananaidu418@users.noreply.github.com>
2020-10-16 21:44:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { v4 as newId } from "uuid";
+
+import Errormsg from "./Components/Errormsg/Errormsg";
+
+export const Theme = React.createContext();
+
+const App = (props) => {
+  const [error, setError] = useState([
+    {
+      nodename: "text node",
+      errorId: newId(),
+      message: "Value is 'null'.Value should not be empty",
+      selected: false,
+    },
+    {
+      nodename: "text node",
+      errorId: newId(),
+      message:
+        "Value is 'null'.Example of a longer error message which takes more than one row",
+      selected: false,
+    },
+    {
+      nodename: "text node",
+      errorId: newId(),
+      message: "Value is 'null'.Value should not be empty",
+      selected: false,
+    },
+    {
+      nodename: "text node",
+      errorId: newId(),
+      message: "Value is 'null'.Value should not be empty",
+      selected: false,
+    },
+  ]);
+  const [isopen, setIsopen] = useState(true);
+  // const[selectedItems,setSelectedItems] = useState([])
+  const [theme, setTheme] = useState("white");
+  // const items = [];
+  const Selection = (id) => {
+    let tempstate = [...error];
+    for (let i = 0; i < tempstate.length; i++) {
+      if (tempstate[i].errorId === id) {
+        tempstate[i].selected = !tempstate[i].selected;
+      }
+    }
+    setError(tempstate);
+  };
+  const handleDropdownChanges = (e) => {
+    setTheme(e.target.value);
+  };
+  const onSubmit = () => {
+    let items = [];
+
+    // if (error.length === 0) {
+    //   alert("No items are selected");
+    // }
+
+    error.forEach((per) => {
+      if (per.selected === true) {
+        items.push(per.errorId);
+      }
+    });
+
+    alert(items.join(","));
+  };
+
+  return (
+    <div className="app">
+      <label >Choose a theme:</label>
+
+      <select
+        onChange={(e) => handleDropdownChanges(e)}
+        
+      >
+        <option value="default">default</option>
+        <option value="white">Light</option>
+        <option value="black">Dark</option>
+      </select>
+      {isopen ? (
+        <div className="popup">
+          <div className="top">
+            <h1>Validation Results</h1>
+          </div>
+          <div className="middle">
+            {error.map((per) => {
+              return (
+                <Theme.Provider value={theme}>
+                  <Errormsg
+                    {...per}
+                    //key={per.errorId}
+
+                    Selection={Selection}
+                  />
+                </Theme.Provider>
+              );
+            })}
+          </div>
+          <div className="close">
+            <div className="btn">
+              <p onClick={() => setIsopen(false)}>close</p>
+            </div>
+            <div className="btn submitBtn" onClick={onSubmit}>
+              <p>submit</p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Errormsg/Errormsg.js
@@ -0,0 +1,58 @@
+import React ,{useState,useContext}from "react";
+import "./Errormsg.css";
+
+import Theme from  "../../App";
+
+const Errormsg =(props)=> {
+    // const [errormessage,setErrormessage] = useState("Errormessage");
+    const [button,setButton] = useState("button");
+    const [onhover,setOnhover] = useState(false);
+
+  const onMouseEnter = () => {
+    setOnhover(true);
+  }
+   const onMouseLeave = () => {
+    setOnhover(false );
+   }
+  
+
+  const alertMessage = () => {
+    alert(props.errorId);
+  };
+ 
+  let color = useContext(Theme);
+  // alert(color)
+    return (
+      <div
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        className={
+          onhover ? "error-message mouseOverBg" : "error-message"
+        }
+      >
+        <div className="error-details" style ={{backgroundColor:color}}>
+          <div className="logo">
+          <input  onClick = {()=>props.Selection(props.errorId)} type="checkbox" className="myCheck"></input>
+            <span class="material-icons">text_rotation_angleup</span>
+          </div>
+          <div className="error">
+            <p className="message-header">{props.nodename}</p>
+            <p className="message-id">{props.errorId}</p>
+          </div>
+          {/* <div>
+            <input  onClick = {()=>this.props.Selection(this.props.errorId)} type="checkbox" className="myCheck"></input>
+          </div> */}
+          {onhover ? (
+            <div className={button}>
+              <p onClick={alertMessage}>open</p>
+            </div>
+          ) : ("" )}
+        </div>
+        <div className="content">
+          <p>{props.message}</p>
+        </div>
+      </div>
+    );
+  
+}
+export default Errormsg;
